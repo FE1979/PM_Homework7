6bd9f542deb6d5ca1e54d4811f6b9b594b8eb6cf
FE1979 <ronin911@gmail.com>
2022-02-12 00:36:42 +0200

    Debouncing with time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-const { getUser, getRepos, getFollowers } = require("./get_user");
+const { getUser } = require('./get_user');
+const { renderUser } = require('./render')
 
-const nopic = './img/nopic.png';
 let userInfo = null;
 
 const pop = document.querySelector('.found')
 const searchBtn = document.getElementById('button-search');
 const searchInput = document.getElementById('user-search');
+
+const debounce = (fn, time=500) => {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn.apply(this, args), time)
+    }
+}
+
+searchUserLive = debounce(searchUserLive);
 
 searchInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
@@ -55,92 +65,4 @@ function searchUserLive() {
                 pop.textContent = 'No suggestions'
             }
         })
-}
-
-function renderUser(userInfo) {
-    renderUserInfo(userInfo);
-
-    getRepos(userInfo.repos_url)
-        .then(repos => renderRepos(repos));
-
-    getFollowers(userInfo.followers_url)
-        .then(followers => renderFollowers(followers))
-}
-
-function renderUserInfo(userInfo) {
-    const avatar = document.querySelector('.user-repos img')
-    const title = document.querySelector('.user-repos .card-title a');
-    const followersTitle = document.querySelector('.user-followers .card-title');
-
-    avatar.src = userInfo.avatar_url || nopic;
-
-    title.textContent = userInfo.login;
-    title.href = userInfo.html_url;
-
-    followersTitle.textContent = `${userInfo.login} followers`;
-}
-
-function renderRepos(repos) {
-    const container = document.querySelector('.repos-list');
-    while (container.firstChild) {
-        container.firstChild.remove()
-    }
-
-    if (repos.length === 0) {
-        const listItem = document.createElement('li');
-        listItem.textContent = 'No repositories found';
-        container.appendChild(listItem);
-        return;
-    }
-
-    const reposList = repos.map(repo => {
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-group-item');
-
-        const link = document.createElement('a');
-        link.textContent = repo.name;
-        link.href = repo.html_url;
-        link.target = "_blank"
-
-        listItem.appendChild(link);
-        return listItem;
-    });
-
-    reposList.forEach(repo => container.appendChild(repo))
-}
-
-function renderFollowers(followers) {
-    const container = document.querySelector('.followers-list');
-    while (container.firstChild) {
-        container.firstChild.remove()
-    }
-
-    if (followers.length === 0) {
-        const listItem = document.createElement('li');
-        listItem.textContent = 'No followers found';
-        container.appendChild(listItem);
-        return;
-    }
-
-    const followersList = followers.map(follower => {
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-group-item');
-
-        const avatar = document.createElement('img');
-        avatar.classList.add('img-thumbnail');
-        avatar.classList.add('w-7');
-
-        avatar.src = follower.avatar_url || nopic;
-
-        const link = document.createElement('a');
-        link.textContent = follower.login;
-        link.href = follower.html_url;
-        link.target = "_blank"
-
-        listItem.appendChild(avatar);
-        listItem.appendChild(link);
-        return listItem;
-    });
-
-    followersList.forEach(follower => container.appendChild(follower))
 }
